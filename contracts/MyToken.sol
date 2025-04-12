357fda977357c08a6fb9f439670f6b434bd76ca5
RangKoon <wag00001@o.cnu.ac.kr>
2025-04-12 19:31:25 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,11 +9,16 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal,
+        uint256 _amount
+    ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(1 * 10 ** uint(decimals), msg.sender); // 1 MT
+        _mint(_amount * 10 ** uint(decimals), msg.sender); // 1 MT
     }
 
     function _mint(uint256 amount, address owner) internal {
@@ -21,15 +26,10 @@ contract MyToken {
         balanceOf[owner] += amount;
     }
 
-    // function totalSupply() external view returns (uint256) {
-    //     return totlaSupply;
-    // }
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
 
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf(owner);
-    // }
-
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
 }
